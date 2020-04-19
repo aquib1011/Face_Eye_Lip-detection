fdeafe017722a05247a640a41ece73be6180a03d
Aquib iqbal <aquib.iq.1011@gmail.com>
2020-04-19 22:58:56 +0530

    Update and rename Code.py to Source_Code.py

--- Source_Code.py
+++ Source_Code.py
@@ -1,10 +1,11 @@
-import cv2
 
-cap = cv2.VideoCapture(0)
+import cv2 #Install OpenCV
+
+cap = cv2.VideoCapture(0) #To use Webcam
 cap.set(3, 640)
 cap.set(4, 480)
 cap.set(10, 100)
-
+#Haarcascade files for detection of a feature
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 lips_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
@@ -14,7 +15,8 @@ while True:
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 6)
-
+    
+    # For creating rectangle around the specified feature
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
         roi_gray = gray[y:y + h, x:x + w]
